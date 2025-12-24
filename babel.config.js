d0f389bafc4029fbdd52f56b4fcfd444218a35be
Rodrigo César <rodrigocnn@gmail.com>
2025-12-24 13:34:40 -0300

    chore: update babel config

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,9 @@
 module.exports = {
-  presets: ["next/babel"],
+  presets: [
+    "next/babel", // mantém a configuração Next.js
+    [
+      "@babel/preset-react", // adiciona runtime automático
+      { runtime: "automatic" },
+    ],
+  ],
 };
