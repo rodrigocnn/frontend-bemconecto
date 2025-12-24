78e1ac72873250e4aaec3dc99a062274b01fcc09
Rodrigo César <rodrigocnn@gmail.com>
2025-12-24 13:09:49 -0300

    test:(modules-sessions) add unit tests to  custom hooks of  sessions module: useDelete and useForm

---
+++ src/modules/sessions/hooks/__tests__/useFormSession.test.tsx
@@ -0,0 +1,111 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { act, renderHook } from "@testing-library/react";
+import { useFormSession } from "../useFormSesssion";
+import * as validations from "../../validations/";
+import { persistMapperSession, updateMapperSession } from "../../mappers";
+
+const queryClient = new QueryClient();
+
+const wrapper = ({ children }: { children: React.ReactNode }) => (
+  <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+);
+
+jest.mock("next/router", () => ({
+  useRouter: jest.fn().mockReturnValue({
+    push: jest.fn(),
+    replace: jest.fn(),
+    pathname: "/",
+    query: {},
+    asPath: "/",
+  }),
+}));
+
+const fakeEvent = {
+  target: { value: "novo valor" },
+} as React.ChangeEvent<HTMLTextAreaElement>;
+
+const form = {
+  summary: "resume of session",
+};
+
+const mockMutate = jest.fn();
+
+jest.mock("../useCreateSession", () => ({
+  useCreateSession: () => ({
+    mutate: mockMutate, // Aqui você consegue espiar se foi chamado
+  }),
+}));
+
+jest.mock("../useEditSession", () => ({
+  useEditSession: () => ({
+    mutate: mockMutate,
+  }),
+}));
+
+describe("useFormSession", () => {
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
+
+  it("Should handleChange form correctly", () => {
+    const { result } = renderHook(() => useFormSession("1", "create"), {
+      wrapper,
+    });
+
+    act(() => {
+      result.current.handleChange(fakeEvent, "summary");
+    });
+
+    expect(result.current.form.summary).toBe("novo valor");
+  });
+
+  it("Should in mode create save session correctly", async () => {
+    jest.spyOn(validations, "sessionValidation").mockResolvedValue(true);
+
+    const { result } = renderHook(() => useFormSession("1", "create"), {
+      wrapper,
+    });
+
+    act(() => {
+      result.current.setForm(form);
+    });
+
+    await act(async () => {
+      await result.current.saveSession();
+    });
+
+    persistMapperSession(result.current.form, "1");
+
+    expect(mockMutate).toHaveBeenCalledWith(
+      expect.objectContaining({
+        patientId: "1",
+        summary: "resume of session",
+        status: "COMPLETED",
+      })
+    );
+  });
+
+  it("Should in mode edit save session correctly", async () => {
+    jest.spyOn(validations, "sessionValidation").mockResolvedValue(true);
+
+    const { result } = renderHook(() => useFormSession("1", "edit"), {
+      wrapper,
+    });
+
+    act(() => {
+      result.current.setForm(form);
+    });
+
+    await act(async () => {
+      await result.current.saveSession();
+    });
+
+    updateMapperSession(result.current.form);
+
+    expect(mockMutate).toHaveBeenCalledWith(
+      expect.objectContaining({
+        summary: "resume of session",
+      })
+    );
+  });
+});
